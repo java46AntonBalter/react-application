f90c334f612758dbcda659cf0ca24f2b0edcce70
Anton Balter <antbalter@gmail.com>
2022-04-07 18:32:45 +0300

    HW#34

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import './App.css';
 import Timer from './components/Timer'
 
 function App() {
-  return <Timer interval={1000}/>
+  const res = (
+  <div className="main">   
+  <Timer city="Jerusalem" locales="en-Us" timeZone='Asia/Jerusalem' interval={1000}/>
+  <Timer city="Tokyo" locales="en-Us" timeZone='Asia/Tokyo' interval={1000}/>
+  <Timer city="London" locales="en-Us" timeZone='Europe/London' interval={1000}/>
+  <Timer city="New York" locales="en-Us" timeZone='America/New_York' interval={1000}/>
+  </div>
+  )
+  return res;
 }
 
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,33 +1,22 @@
 import React, { useEffect } from 'react';
 type Props = {
-    interval: number
+    city: string,
+    locales: string,
+    timeZone: string,
+    interval?: number
 }
-const Timer: React.FC<Props> = ({interval}) => {
+const Timer: React.FC<Props> = ({city, locales, timeZone, interval}) => {
     const [time, setTime] = React.useState(new Date());
     function tic():void {
         setTime(new Date());
     }
     useEffect(() => {
         setInterval(tic, interval || 1000);
-    }, [])
+    }, [interval])
 
-    return <div className="main">
-        <div className="time-element">
-            <h3>Jerusalem:</h3>
-            <div className="time"><label>{time.toLocaleTimeString('en-US', { timeZone: 'Asia/Jerusalem' })}</label></div>
+    return <div className="time-element">
+            <h3>{city}:</h3>
+            <div className="time">{time.toLocaleTimeString( locales, { timeZone: timeZone })}</div>
         </div>
-        <div className="time-element">
-            <h3>Tokyo:</h3>
-            <div className="time"><label>{time.toLocaleTimeString('en-US', { timeZone: 'Asia/Tokyo' })}</label></div>
-        </div>
-        <div className="time-element">
-            <h3>London:</h3>
-            <div className="time"><label>{time.toLocaleTimeString('en-US', { timeZone: 'Europe/London' })}</label></div>
-        </div>
-        <div className="time-element">
-            <h3>New York:</h3>
-            <div className="time"><label>{time.toLocaleTimeString('en-US', { timeZone: 'America/New_York' })}</label></div>
-        </div>
-    </div>
 }
 export default Timer;
