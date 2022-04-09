3b7b95d277b2ba44770c17ee1759ac0da82b4c27
Anton Balter <antbalter@gmail.com>
2022-04-09 20:49:43 +0300

    HW#35

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Color from './components/Color';
+import InputData from './components/InputData';
+import timeZones from './config/time-zones';
+import Timer from './components/Timer';
+import { HexColorPicker } from "react-colorful";
+import './App.css'
 
+type ComponentNames = "select timezone" | "timer" | "color" | "select color"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [timeZone, setTimeZone] = React.useState("Asia/Jerusalem");
+const [color, setColor] = React.useState("red");
+const [componentName, setComponentName] = React.useState<ComponentNames>('select timezone');
+const mapComponents: Map<ComponentNames, React.ReactNode> = new Map();
+mapComponents.set("select color", <HexColorPicker color={color} onChange={setColor} />)
+mapComponents.set("color", <Color color={color}></Color>);
+mapComponents.set("select timezone", <InputData timeZones={timeZones} injectTimeZone={setTimeZone}></InputData>);
+mapComponents.set("timer", <Timer timeZone={timeZone}></Timer>);
+
+  return  <div className="main" style={{display:"flex",  alignItems:"center", flexDirection:"column"}}>
+    <div style={{display:"flex",  alignItems:"center", flexDirection:"row", padding:"3vw"}}>
+    {Array.from(mapComponents.keys()).map(k => <button className="button" style={{margin:"0.5vw"}} onClick={() => setComponentName(k)}>{k}</button>)}
     </div>
-  );
+    <br/>
+    {mapComponents.get(componentName)}
+    </div>
 }
 
 export default App;
